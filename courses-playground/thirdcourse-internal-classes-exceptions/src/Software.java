305ecabb3cc1569cf1f0ae01ace3de66d81dfb1d
theod <theodor.ciobanoiu@gmail.com>
2022-07-18 12:40:31 +0300

    Second + third course

---
+++ courses-playground/thirdcourse-internal-classes-exceptions/src/Software.java
@@ -0,0 +1,13 @@
+public class Software extends Product {
+    private int size;
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+    
+    
+}
