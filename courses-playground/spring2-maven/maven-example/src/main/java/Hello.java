bb7910783f703f79e6b29a5bb96ec312f0dc3721
theod <theodor.ciobanoiu@gmail.com>
2022-07-27 11:02:21 +0300

    Added

--- courses-playground/spring2-maven/maven-example/src/main/java/Hello.java
+++ courses-playground/spring2-maven/maven-example/src/main/java/Hello.java
@@ -4,5 +4,6 @@ import org.apache.commons.lang3.tuple.Pair;
 public class Hello {
     public static void main(String[] args) {
         Pair pair = new ImmutablePair<String, String>("test", "test");
+
     }
 }
