7f010616bfbf1b1065a14ba9341ae4cc4269e562
theod <theodor.ciobanoiu@gmail.com>
2022-08-02 16:24:32 +0300

    Added Spring Boot 2 project

---
+++ courses-playground/boot2proj/src/test/java/com/spring/boot2/boot2proj/Boot2projApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot2.boot2proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot2projApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
